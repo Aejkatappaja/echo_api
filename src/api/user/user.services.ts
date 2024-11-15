3073d97c066d84053e6f951e9a056de9158a03fd
Aejkatappaja <joverjlj@gmail.com>
2024-11-15 22:35:05 +0100

    refactor(user_services): improve readability

--- src/api/user/user.services.ts
+++ src/api/user/user.services.ts
@@ -1,6 +1,4 @@
-/* eslint-disable no-useless-catch */
-
-import { RESET_PASSWORD_URL } from '@/core/constants';
+import { RESET_PASSWORD_URL, TOKEN_EXPIRATION } from '@/core/constants';
 import type { IUserDocument, IUserModel, UserSchemaType } from '@/database';
 import { User } from '@/database';
 import { sendMail } from '@/utils/mail';
@@ -14,27 +12,16 @@ class UserService {
     this.userModel = userModel;
   }
 
-  async #existingEmail(email: string): Promise<boolean | Error> {
-    try {
-      const user = await this.userModel.getUserByEmail(email);
-      return !!user;
-    } catch (err: unknown) {
-      console.error(err);
-      throw new Error('Error checking email existence');
-    }
+  private async existingEmail(email: string): Promise<void> {
+    const user = await this.userModel.getUserByEmail(email);
+    if (user) throw new Error('Email already in use');
   }
 
   public async create(creationFields: UserSchemaType): Promise<IUserDocument | void> {
-    try {
-      const email = creationFields.email.toLowerCase();
-      const emailAlreadyUsed = await this.#existingEmail(email);
-      if (emailAlreadyUsed) throw new Error('Email already in use');
-      const password = await passwordUtils.hash(creationFields.password);
-      const newUser = await this.userModel.createUser({ ...creationFields, email, password });
-      return newUser;
-    } catch (err) {
-      throw err;
-    }
+    const email = creationFields.email.toLowerCase();
+    await this.existingEmail(email);
+    const password = await passwordUtils.hash(creationFields.password);
+    return await this.userModel.createUser({ ...creationFields, email, password });
   }
 
   public async forgotPassword(email: string): Promise<IUserDocument | void> {
@@ -42,8 +29,7 @@ class UserService {
     if (!user) throw new Error('User not found');
     const token = tokenUtils.generateToken();
     console.log(token);
-    user.resetPasswordToken = token;
-    user.resetPasswordExpires = new Date(Date.now() + 5 * 60 * 1000);
+    await this.managePasswordTokenFields(user, token);
     await user.save();
 
     const resetURL = `${RESET_PASSWORD_URL}/${token}`;
@@ -51,18 +37,28 @@ class UserService {
     await sendMail(
       email,
       'Password Reset',
-      `You requested a password reset. Click the link to reset your password: ${resetURL}`
+      `You requested a password reset. Click the link to reset your password: ${resetURL}`,
     );
   }
 
-  public async resetPassword(resetPasswordFields: { token: string; password: string }) {
+  private async managePasswordTokenFields(user: IUserDocument, token?: string): Promise<void> {
+    if (token) {
+      user.resetPasswordToken = token;
+      user.resetPasswordExpires = new Date(Date.now() + TOKEN_EXPIRATION); // 5 minutes
+    } else {
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpires = undefined;
+    }
+  }
+
+  public async resetPassword(resetPasswordFields: { token: string; password: string }): Promise<void> {
     const user = await this.userModel.getUserByToken(resetPasswordFields.token);
     if (!user) throw new Error('Invalid or expired token');
-    const hashedPassword = await passwordUtils.hash(resetPasswordFields.password);
-    user.password = hashedPassword;
-    user.resetPasswordToken = undefined;
-    user.resetPasswordExpires = undefined;
-    this.userModel.unlockAccount(user._id.toString());
+
+    user.password = await passwordUtils.hash(resetPasswordFields.password);
+    await this.managePasswordTokenFields(user);
+
+    if (user.locked) this.userModel.unlockAccount(user._id.toString());
 
     await user.save();
   }
