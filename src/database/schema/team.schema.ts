d2f522849c11dd7018341ffee1f3bedf42c1ca63
Aejkatappaja <joverjlj@gmail.com>
2024-11-23 22:22:27 +0100

    refactor(team.schema): add boards property

--- src/database/schema/team.schema.ts
+++ src/database/schema/team.schema.ts
@@ -6,6 +6,7 @@ export const zodTeamSchema = z.object({
   name: z.string().min(3),
   admin: z.instanceof(Schema.Types.ObjectId),
   members: z.array(z.instanceof(Schema.Types.ObjectId)).optional(),
+  boards: z.array(z.instanceof(Schema.Types.ObjectId)).optional(),
 });
 
 export type TeamSchemaType = z.infer<typeof zodTeamSchema>;
@@ -22,6 +23,7 @@ const TeamSchema = new Schema<ITeamDocument>({
   name: { type: String, required: true },
   admin: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   members: { type: [Schema.Types.ObjectId], ref: 'User', required: false },
+  boards: { type: [Schema.Types.ObjectId], ref: 'Board', required: false },
 });
 
 TeamSchema.statics.createTeam = async function (createFields: ITeamDocument) {
